639d459b790f83e5f3683f8d1a8dcc72e0e0b48b
Abel Bima Wiratama <learn.smktelkom-mlg.sch.id>
2016-09-25 16:28:51 +0700

    Kita membuat edit text dengan cara progamming

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
@@ -2,6 +2,8 @@ package id.sch.smktelkom_mlg.learn.advancedwidget1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout llMain = (LinearLayout) findViewById(R.id.linearLayoutMain);
+
+        EditText etNama = new EditText(this);
+        llMain.addView(etNama);
+        etNama.setHint("Isikan Nama Anak");
     }
 }
